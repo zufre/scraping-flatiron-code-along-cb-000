a05a5b8f6e4e59497b3fb10cd82432cd62e45916
Cedric David <cedric.david@btopenworld.com>
2018-09-08 02:31:07 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,17 +1,14 @@
 class Course
-  attr_accessor :title, :schedule, :description
-
-
-  @@all = []
+  attr_accessor :title ,:schedule, :description
+  @@all =[]
 
   def initialize
     @@all << self
-  end
 
+  end
   def self.all
     @@all
   end
-
   def self.reset_all
     @@all.clear
   end
