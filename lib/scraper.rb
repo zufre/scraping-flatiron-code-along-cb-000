cef0bcdc020c911ebe714b46f1ce2909b96aef1d
Cedric David <cedric.david@btopenworld.com>
2018-09-08 03:02:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,6 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
-
     end
   end
   binding.pry
