ff9d45cd39cc05ca3adadbf31285d92a327cc9b8
Cedric David <cedric.david@btopenworld.com>
2018-09-08 03:10:12 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,5 +25,16 @@ class Scraper
     end
   
   end
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
 Scraper.new.get_page
